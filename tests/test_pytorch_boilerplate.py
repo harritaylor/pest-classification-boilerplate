0d60078f21ea4ebf2de42bb9d1f1f03644fca3c5
Harri Taylor <harritaylor@pm.me>
2022-05-02 10:20:28 +0100

    added CUDA test

--- tests/test_pytorch_boilerplate.py
+++ tests/test_pytorch_boilerplate.py
@@ -3,3 +3,8 @@ from pytorch_boilerplate import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cuda_available():
+    import torch
+
+    assert torch.cuda.is_available() == True
